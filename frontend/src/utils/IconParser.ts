47bab54910f62e66caa6ca40a668215ee4e67f2b
Dadzial <duszakdamian034@gmail.com>
2026-03-08 22:42:58 +0100

    add icons

--- frontend/src/utils/IconParser.ts
+++ frontend/src/utils/IconParser.ts
@@ -1,5 +1,6 @@
 export const IconsParser = {
-
+    homeIcon : require('../assets/home.png'),
+    airIcon : require('../assets/air.png'),
 };
 
 export const getIcon = (name: keyof typeof IconsParser) => IconsParser[name];
